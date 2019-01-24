ba6db4ee44d603d41d8bc5944d6a780d9005a151
Amr Hendy <amr.m.hendy@gmail.com>
2019-01-25 01:00:36 +0200

    Handled the random grid to have path always

--- Model/maze.py
+++ Model/maze.py
@@ -1,4 +1,7 @@
 from random import shuffle
+
+from numpy.core.multiarray import dtype
+
 from Model.state import State
 from Model.states_map import StateMap
 import numpy as np
@@ -25,13 +28,18 @@ class Maze:
             cells with value = one means barrier cells
             cells with value = zero means empty cells
         """
-        barrier_count = int(self.__maze_rows * self.__maze_cols * 0.2)
-        grid = [1] * barrier_count + [0] * (self.__maze_rows * self.__maze_cols - barrier_count)
-        shuffle(grid)
-        # mark the start and the end
-        grid[0] = 0
-        grid[-1] = 0
-        grid = np.reshape(grid, (self.__maze_rows, self.__maze_cols))
+        valid_grid = False
+        while not valid_grid:
+            print 'Generating Random Grid'
+            blocks_ratio = 0.2
+            barrier_count = int(self.__maze_rows * self.__maze_cols * blocks_ratio)
+            grid = [1] * barrier_count + [0] * (self.__maze_rows * self.__maze_cols - barrier_count)
+            shuffle(grid)
+            # mark the start and the end
+            grid[0] = 0
+            grid[-1] = 0
+            grid = np.reshape(grid, (self.__maze_rows, self.__maze_cols))
+            valid_grid = self.__is_valid(grid)
         return grid
 
     def get_state(self, row, col):
@@ -54,3 +62,30 @@ class Maze:
 
     def get_agent_index(self):
         return self.__agent_index
+
+    def __is_valid(self, grid):
+        vis = np.zeros(shape=grid.shape, dtype='int')
+        q = list()
+        q.append((0, 0))
+        while len(q) != 0:
+            row, col = q.pop(0)
+            if vis[row, col] == 1:
+                continue
+            vis[row, col] = 1
+            # check we reach the end cell
+            if row == self.__maze_rows - 1 and col == self.__maze_cols - 1:
+                return True
+            # expand to children
+            dx = [-1, 1, 0, 0]
+            dy = [0, 0, -1 , 1]
+            for i in range(4):
+                new_row, new_col = row + dx[i], col + dy[i]
+                if self.__is_valid_index(new_row, new_col) and vis[new_row, new_col] == 0 and grid[new_row, new_col] == 0:
+                    q.append((new_row, new_col))
+        return False
+
+    def __is_valid_index(self, row, col):
+        if row < 0 or row >= self.__maze_rows or col < 0 or col >= self.__maze_cols:
+            return False
+        return True
+    
